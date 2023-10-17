f131a57937ea7c9f5336c50188d2e7aa0a6a76ce
fredrikstrandberg <fredrik.strandberg@live.se>
2023-10-17 18:00:10 +0200

    commit

--- lib/account/handle_login.dart
+++ lib/account/handle_login.dart
@@ -1,24 +1,48 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/pop_up.dart';
 import 'account.dart';
 import 'account_list.dart';
 
 
-HandleLogin(context, String email, String password) {
+HandleLogin(context, email, password) {
 
   if (accountMap[email] == password) {
     Navigator.pushReplacementNamed(context, "/main");
   }
+  else if (accountMap[email] == null) {
+    return HandleLoginError(context, "Email är inte registrerad");
+  }
   else {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) =>
-            const AlertDialog(
-              title: Text("Inloggning misslyckades!"
+    return HandleLoginError(context, "Felaktigt lösenord");
+  }
+}
+
+HandleLoginError(context, errorString) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) =>
+    AlertDialog(
+      title: Text(
+          errorString,
+      ),
+      titleTextStyle: const TextStyle(
+        letterSpacing: 1,
+        fontFamily: "YoungSerif",
+        fontSize: 15,
+        color: Colors.black,
+      ),
+      actions: [
+        TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              "Ok",
+              style: TextStyle(
+              fontFamily: "YoungSerif"
               ),
             ),
-    );
-  }
+        )
+      ],
+    ),
+  );
 }
